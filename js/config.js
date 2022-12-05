9daa725cefc46419ca67d4686c2d1b01757e3090
Lastedsky <58127605+Lastedsky@users.noreply.github.com>
2022-12-04 23:51:33 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId:         'us-east-2_UvXtNgbM5',         // e.g. us-east-2_uXboG5pAb
         userPoolClientId:   'g534sfb1fbr47sgh41mevn3md',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             ''          // e.g. us-east-2
+        region:             'us-east-2'          // e.g. us-east-2
     },
     api: {
         invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
